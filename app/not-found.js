461af75d54d2b7a112c06f9b27b76c31db536331
haikali3 <manfdvcL9@gmail.com>
2023-12-15 16:16:13 +0800

    add error page

---
+++ app/not-found.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NotFound = () => {
+  return (
+    <main className="not-found">
+      <h1>Not found</h1>
+      <p>Sorry, we couldn&apos;t find what you were looking for.</p>
+    </main>
+  );
+};
+
+export default NotFound;
